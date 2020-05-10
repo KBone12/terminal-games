d5bd95076f8c88306a8ea8491b7579776c8543be
kbone <kbonehobby@gmail.com>
2020-05-10 12:48:40 +0900

    Use Bernoulli distribution

--- minesweeper/src/main.rs
+++ minesweeper/src/main.rs
@@ -1,4 +1,7 @@
-use rand;
+use rand::{
+    self,
+    distributions::{Bernoulli, Distribution},
+};
 
 #[derive(Clone, Copy, PartialEq)]
 enum CellState {
@@ -6,31 +9,19 @@ enum CellState {
     Bomb { hidden: bool },
 }
 
-fn print_board<R: AsRef<[CellState]>>(board: &[R]) {
-    board.iter().for_each(|row| {
-        row.as_ref().iter().for_each(|cell| match cell {
-            CellState::Empty { count, hidden } => {
-                if *hidden {
-                    print!("*");
-                } else {
-                    print!("{}", count);
-                }
-            }
-            CellState::Bomb { hidden } => {
-                if *hidden {
-                    print!("*");
-                } else {
-                    print!("X");
-                }
-            }
-        });
-        println!("");
-    });
+impl CellState {
+    pub fn is_empty(&self) -> bool {
+        matches!(self, CellState::Empty { .. })
+    }
+    pub fn is_bomb(&self) -> bool {
+        matches!(self, CellState::Bomb { .. })
+    }
 }
 
-fn main() {
-    let width = 8;
-    let height = 8;
+fn generate_board(width: usize, height: usize, rate: f64, max_bombs: usize) -> Vec<Vec<CellState>> {
+    let rng = rand::thread_rng();
+    let mut samples = Bernoulli::new(rate).unwrap().sample_iter(rng);
+    let mut bombs = 0;
     let mut board = vec![
         vec![
             CellState::Empty {
@@ -41,40 +32,38 @@ fn main() {
         ];
         height
     ];
-    let max_bombs = 4;
-    let bomb_rate = max_bombs as f64 / (width * height) as f64;
-    let mut bomb_count = 0;
-    while bomb_count < max_bombs {
+
+    while bombs < max_bombs {
         for y in 0..height {
             for x in 0..width {
-                if matches!(board[y][x], CellState::Bomb { .. }) {
+                if board[y][x].is_bomb() {
                     continue;
                 }
-                if bomb_count < max_bombs && rand::random::<f64>() < bomb_rate {
+                if bombs < max_bombs && samples.next().unwrap() {
                     board[y][x] = CellState::Bomb { hidden: true };
-                    bomb_count += 1;
+                    bombs += 1;
                 }
             }
         }
     }
+
     for y in 0..height {
         for x in 0..width {
-            if matches!(board[y][x], CellState::Bomb { .. }) {
+            if board[y][x].is_bomb() {
                 continue;
             }
             let mut tmp = 0;
-            let x = x as isize;
-            let y = y as isize;
+            let (x, y, width, height) = (x as isize, y as isize, width as isize, height as isize);
             for i in -1..=1 {
                 for j in -1..=1 {
                     if i == 0 && j == 0 {
                         continue;
                     }
                     if y + i >= 0
-                        && y + i < height as isize
+                        && y + i < height
                         && x + j >= 0
-                        && x + j < width as isize
-                        && matches!(board[(y + i) as usize][(x + j) as usize], CellState::Bomb { .. })
+                        && x + j < width
+                        && board[(y + i) as usize][(x + j) as usize].is_bomb()
                     {
                         tmp += 1;
                     }
@@ -88,6 +77,44 @@ fn main() {
             }
         }
     }
+
+    board
+}
+
+fn print_board<R: AsRef<[CellState]>>(board: &[R]) {
+    board.iter().for_each(|row| {
+        row.as_ref().iter().for_each(|cell| match cell {
+            CellState::Empty { count, hidden } => {
+                /*
+                if *hidden {
+                    print!("*");
+                } else {
+                    print!("{}", count);
+                }
+                */
+                print!("{}", count);
+            }
+            CellState::Bomb { hidden } => {
+                /*
+                if *hidden {
+                    print!("*");
+                } else {
+                    print!("X");
+                }
+                */
+                print!("X");
+            }
+        });
+        println!("");
+    });
+}
+
+fn main() {
+    let width = 8;
+    let height = 8;
+    let max_bombs = 4;
+    let bomb_rate = max_bombs as f64 / (width * height) as f64;
+    let board = generate_board(width, height, bomb_rate, max_bombs);
 
     print_board(&board);
 }
